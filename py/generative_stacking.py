45c14becfb84fa57d77793e32538e964973ce8b5
lovill <lorenzo.villaggi@gmail.com>
2019-04-10 18:07:17 +0900

    Remove print message for new branch testing

--- py/generative_stacking.py
+++ py/generative_stacking.py
@@ -16,10 +16,6 @@ import sys
 import os
 sc.doc = ghdoc
 
-print("new branch")
-print("second commit on this new branch")
-print("make new commit for surrogate branch")
-
 curr_dir = os.getcwd()
 py_dir = os.path.join(curr_dir, "py")
 sys.path.append(py_dir)	
